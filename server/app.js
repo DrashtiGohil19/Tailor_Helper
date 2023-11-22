1bbc6892dd3c2728a01c0ab2822d764931f575db
DrashtiGohil19 <gohildrashti636@gmail.com>
2023-11-22 17:48:30 +0530

    changes made

--- server/app.js
+++ server/app.js
@@ -28,17 +28,17 @@ app.use('/bill', billRouter);
 
 // --------- Deployement ------------
 
-const __dirname1 = path.resolve()
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "client/build")))
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-  })
-} else {
-  app.get('/', (req, res) => {
-    res.send("API running succesfully !")
-  })
-}
+// const __dirname1 = path.resolve()
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "client/build")))
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+//   })
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send("API running succesfully !")
+//   })
+// }
 
 // --------- Deployement ------------
 
